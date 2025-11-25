d9a63f14a9431d401e6281c598c2ffa6c7c78965
SeeTwoo <walti.bee@gmail.com>
2025-11-25 19:30:39 +0100

    created unbr struct

--- src/unsigned_base_pf.c
+++ src/unsigned_base_pf.c
@@ -3,48 +3,61 @@
 #include "pf_struct.h"
 #include "spec_struct.h"
 
+typedef struct s_unbr
+{
+	unsigned int	n;
+	int				len;
+	char			buf[11];
+	char const		*base;
+	unsigned int	div;
+}	t_unbr;
+
 static const char	g_base_ten[11] = "0123456789";
 static const char	g_low_hex_base[17] = "0123456789abcdef";
 static const char	g_up_hex_base[17] = "0123456789ABCDEF";
 
 int	pf_realloc(t_pf *pf, size_t n);
 
-int	put_u_nbr_base(char *buf, unsigned int n, char const *base)
+static int	put_u_nbr_base(t_unbr *unbr)
 {
 	int				i;
-	unsigned int	div;
 
-	div = strlen(base);
 	i = 10;
-	while (n >= div)
+	while (unbr->n >= unbr->div)
 	{
-		buf[i] = base[n % div];
-		n /= div;
+		unbr->buf[i] = unbr->base[unbr->n % unbr->div];
+		unbr->n /= unbr->div;
 		i--;
 	}
-	buf[i] = base[n];
+	unbr->buf[i] = unbr->base[unbr->n];
 	i--;
 	return (10 - i);
 }
 
+static void	init_unsigned(t_spec spec, t_unbr *unbr)
+{
+	if (spec.type == 'u')
+		unbr->base = &g_base_ten[0];
+	else if (spec.type == 'x')
+		unbr->base = &g_low_hex_base[0];
+	else
+		unbr->base = &g_up_hex_base[0];
+	if (spec.type == 'u')
+		unbr->div = 10;
+	else
+		unbr->div = 16;
+}
+
 int unsigned_base_pf(t_pf *pf, t_spec spec)
 {
-	unsigned int	n;
-	int				len;
-	char			buf[11];
-	char const		*base;
+	t_unbr	unbr;
 
-	if (spec.type == 'u')
-		base = &g_base_ten[0];
-	else if (spec.type == 'x')
-		base = &g_low_hex_base[0];
-	else
-		base = &g_up_hex_base[0];
-	n = va_arg(pf->arg, unsigned int);
-	len = put_u_nbr_base(buf, n, base);
-	if (pf_realloc(pf, (size_t)len) == -1)
+	unbr.n = va_arg(pf->arg, unsigned int);
+	init_unsigned(spec, &unbr);
+	unbr.len = put_u_nbr_base(&unbr);
+	if (pf_realloc(pf, (size_t)unbr.len) == -1)
 		return (-1);
-	memcpy(pf->buf, buf + 11 - len, len);
-	pf->len += len;
+	memcpy(pf->buf, unbr.buf + 11 - unbr.len, unbr.len);
+	pf->len += unbr.len;
 	return (0);
 }
