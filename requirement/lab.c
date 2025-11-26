ecf0943ba6974d7dff0d385f51a45804285c04f2
SeeTwoo <walti.bee@gmail.com>
2025-11-26 10:34:06 +0100

    working on a refactoring because it turns out I am repeating myself way too much

--- requirement/lab.c
+++ requirement/lab.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
+#include <stdint.h>
 
 int	main(void)
 {
-	int		n = 42;
+	uintptr_t	n = UINT64_MAX;
+	int			i = 0;
 
-	printf("[%#030.15p]\n", &n);
+	printf("%llu\n", n);
+	while (n > 16) {
+		i++;
+		n /= 16;
+	}
+	i++;
+	printf("uintptr_t max len = %d\n", i);
 	return (0);
 }
