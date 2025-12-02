c45d78f7625531e8348f2db750400ad293cc46c8
SeeTwoo <walti.bee@gmail.com>
2025-12-02 23:50:11 +0100

    added a lot of tests to lohex, all of them pass

--- tester/hexa_tests.c
+++ tester/hexa_tests.c
@@ -24,6 +24,31 @@ Test(x_tests, bonus_lohex)
 	assert_fmt("%#.0x", 0);         // no "0x"
 	assert_fmt("%.1x", 0);
 	assert_fmt("%#.1x", 0);
+	assert_fmt("%#x", 0x42);
+	assert_fmt("%#10x", 0x42);
+	assert_fmt("%#-10x", 0x42);
+	assert_fmt("%#.8x", 0x42);
+	assert_fmt("%#08x", 0x42);
+	assert_fmt("%08x", 0x123);
+	assert_fmt("%.8x", 0x123);
+	assert_fmt("%08.3x", 0x123);
+	assert_fmt("%08.10x", 0x123);
+	assert_fmt("%+x", 0x123);
+	assert_fmt("% x", 0x123);
+	assert_fmt("% +10x", 0x123);
+	assert_fmt("% +.5x", 0x123);
+	assert_fmt("%1x", 0xF);
+	assert_fmt("%2x", 0xF);
+	assert_fmt("%20x", 0xF);
+	assert_fmt("%-1x", 0xF);
+	assert_fmt("%-20x", 0xF);
+	assert_fmt("%020x", 0xF);
+	assert_fmt("%X", 0xDEADBEEF);
+	assert_fmt("%#X", 0x123);
+	assert_fmt("%#10X", 0x123);
+	assert_fmt("%08X", 0xABCD);
+	assert_fmt("%.30x", 0x123);
+	assert_fmt("%#.30x", 0x123);
 }
 
 Test(X_tests, mandatory_uphex)
