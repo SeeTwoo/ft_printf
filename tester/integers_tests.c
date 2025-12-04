823ec8e8f9fa775ca4368de062ed76feb01af3e5
SeeTwoo <walti.bee@gmail.com>
2025-12-04 20:23:04 +0100

    more tests

--- tester/integers_tests.c
+++ tester/integers_tests.c
@@ -21,6 +21,7 @@ Test(d_tests, null_no_precision)
 	assert_fmt("%2d", -579103);
 	assert_fmt("%6d", -579103);
 	assert_fmt("%8d", -579103);
+	assert_fmt("%08d", -579103);
 }
 
 Test(d_tests, null_zero_precision)
@@ -30,6 +31,7 @@ Test(d_tests, null_zero_precision)
 	assert_fmt("%2.0d", -579103);
 	assert_fmt("%6.0d", -579103);
 	assert_fmt("%8.0d", -579103);
+	assert_fmt("%08.0d", -579103);
 }
 
 Test(d_tests, null_little_precision)
@@ -39,6 +41,7 @@ Test(d_tests, null_little_precision)
 	assert_fmt("%2.2d", -579103);
 	assert_fmt("%6.2d", -579103);
 	assert_fmt("%8.2d", -579103);
+	assert_fmt("%08.2d", -579103);
 }
 
 Test(d_tests, null_right_precision)
@@ -48,6 +51,7 @@ Test(d_tests, null_right_precision)
 	assert_fmt("%2.6d", -579103);
 	assert_fmt("%6.6d", -579103);
 	assert_fmt("%8.6d", -579103);
+	assert_fmt("%08.6d", -579103);
 }
 
 Test(d_tests, null_large_precision)
@@ -55,18 +59,28 @@ Test(d_tests, null_large_precision)
 	assert_fmt("%.8d", -579103);
 	assert_fmt("%0.8d", -579103);
 	assert_fmt("%2.8d", -579103);
+	assert_fmt("%02.8d", -579103);
 	assert_fmt("%6.8d", -579103);
+	assert_fmt("%06.8d", -579103);
 	assert_fmt("%8.8d", -579103);
+	assert_fmt("%08.8d", -579103);
 }
 
 //zero
 Test(d_tests, empty_no_precision)
 {
 	assert_fmt("%d", 0);
+	assert_fmt("%+d", 0);
+	assert_fmt("%-d", 0);
 	assert_fmt("%0d", 0);
+	assert_fmt("%-0d", 0);
+	assert_fmt("%+0d", 0);
 	assert_fmt("%2d", 0);
-	assert_fmt("%6d", 0);
-	assert_fmt("%8d", 0);
+	assert_fmt("%02d", 0);
+	assert_fmt("%+2d", 0);
+	assert_fmt("%+02d", 0);
+	assert_fmt("%-2d", 0);
+	assert_fmt("%-02d", 0);
 }
 
 Test(d_tests, emtpy_zero_precision)
@@ -74,8 +88,6 @@ Test(d_tests, emtpy_zero_precision)
 	assert_fmt("%.0d", 0);
 	assert_fmt("%0.0d", 0);
 	assert_fmt("%2.0d", 0);
-	assert_fmt("%6.0d", 0);
-	assert_fmt("%8.0d", 0);
 }
 
 Test(d_tests, emtpy_little_precision)
@@ -83,26 +95,6 @@ Test(d_tests, emtpy_little_precision)
 	assert_fmt("%.2d", 0);
 	assert_fmt("%0.2d", 0);
 	assert_fmt("%2.2d", 0);
-	assert_fmt("%6.2d", 0);
-	assert_fmt("%8.2d", 0);
-}
-
-Test(d_tests, emtpy_right_precision)
-{
-	assert_fmt("%.8d", 0);
-	assert_fmt("%0.6d", 0);
- 	assert_fmt("%2.6d", 0);
-	assert_fmt("%6.6d", 0);
-	assert_fmt("%8.6d", 0);
-}
-
-Test(d_tests, emtpy_large_precision)
-{
-	assert_fmt("%.8d", 0);
-	assert_fmt("%0.8d", 0);
-	assert_fmt("%2.8d", 0);
-	assert_fmt("%6.8d", 0);
-	assert_fmt("%8.8d", 0);
 }
 
 //positive
@@ -208,44 +200,27 @@ Test(d_tests, l_empty_no_precision)
 	assert_fmt("%-d", 0);
 	assert_fmt("%-0d", 0);
 	assert_fmt("%-2d", 0);
-	assert_fmt("%-6d", 0);
-	assert_fmt("%-8d", 0);
+	assert_fmt("%0-2d", 0);
 }
 
 Test(d_tests, l_empty_zero_precision)
 {
 	assert_fmt("%-.0d", 0);
+	assert_fmt("%0-.0d", 0);
 	assert_fmt("%-0.0d", 0);
+	assert_fmt("%0-0.0d", 0);
 	assert_fmt("%-2.0d", 0);
-	assert_fmt("%-6.0d", 0);
-	assert_fmt("%-8.0d", 0);
+	assert_fmt("%0-2.0d", 0);
 }
 
 Test(d_tests, l_empty_little_precision)
 {
 	assert_fmt("%-.2d", 0);
+	assert_fmt("%0-.2d", 0);
 	assert_fmt("%-0.2d", 0);
+	assert_fmt("%0-0.2d", 0);
 	assert_fmt("%-2.2d", 0);
-	assert_fmt("%-6.2d", 0);
-	assert_fmt("%-8.2d", 0);
-}
-
-Test(d_tests, l_empty_right_precision)
-{
-	assert_fmt("%-.6d", 0);
-	assert_fmt("%-0.6d", 0);
-	assert_fmt("%-2.6d", 0);
-	assert_fmt("%-6.6d", 0);
-	assert_fmt("%-8.6d", 0);
-}
-
-Test(d_tests, l_empty_large_precision)
-{
-	assert_fmt("%-.8d", 0);
-	assert_fmt("%-0.8d", 0);
-	assert_fmt("%-2.8d", 0);
-	assert_fmt("%-6.8d", 0);
-	assert_fmt("%-8.8d", 0);
+	assert_fmt("%0-2.2d", 0);
 }
 
 //positive
