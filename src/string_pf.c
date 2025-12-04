15ef0b343661fbd7273885e556e7cdde2572349f
SeeTwoo <walti.bee@gmail.com>
2025-12-04 02:09:55 +0100

    added pretty much every tests for s and c

--- src/string_pf.c
+++ src/string_pf.c
@@ -19,22 +19,34 @@
 void	compute_padding(t_arg *arg, t_spec spec);
 size_t	full_len(int len, int width);
 
+int	null_string(t_arg *arg, t_spec spec)
+{
+	arg->raw = "(null)";
+	arg->len = 6;
+	if (spec.precision != -1 && spec.precision < 6)
+		arg->len = 0;
+	arg->prefix_len = 0;
+	arg->zeroes = 0;
+	arg->full_len = arg->len;
+	compute_padding(arg, spec);
+	return (0);
+}
+
 void	str_len_to_display(t_arg *arg, t_spec spec)
 {
 	arg->len = ft_strlen(arg->val.str);
 	if (spec.precision != -1 && arg->len > (size_t)spec.precision)
 		arg->len = spec.precision;
+	arg->full_len = arg->len;
 }
 
 int	string_pf(t_pf *pf, t_spec spec, t_arg *arg)
 {
 	arg->val.str = va_arg(pf->arg, char *);
-	if (!arg->val.str && spec.precision != -1 && spec.precision <= 6)
-		arg->val.str = "";
-	else if (!arg->val.str)
-		arg->val.str = "(null)";
-	str_len_to_display(arg, spec);
+	if (!arg->val.str)
+		return (null_string(arg, spec));
 	arg->raw = arg->val.str;
+	str_len_to_display(arg, spec);
 	compute_padding(arg, spec);
 	return (0);
 }
--- tester/char_tests.c
+++ tester/char_tests.c
@@ -10,9 +10,83 @@ Test(c_tests, mandatory_chars)
     assert_fmt("%c", '\0');
 }
 
-Test(c_tests, bonus_chars)
+Test(c_tests, no_precision)
 {
-	assert_fmt("%5c", 'a');
-	assert_fmt("%.5c", 'a');
+	assert_fmt("%c", 'a');
+	assert_fmt("%0c", 'a');
+	assert_fmt("%1c", 'a');
 	assert_fmt("%5c", 'a');
 }
+
+Test(c_tests, zero_precision)
+{
+	assert_fmt("%.0c", 'a');
+	assert_fmt("%0.0c", 'a');
+	assert_fmt("%1.0c", 'a');
+	assert_fmt("%5.0c", 'a');
+}
+
+Test(c_tests, one_precision)
+{
+	assert_fmt("%.1c", 'a');
+	assert_fmt("%0.1c", 'a');
+	assert_fmt("%1.1c", 'a');
+	assert_fmt("%5.1c", 'a');
+}
+
+Test(c_tests, large_precision)
+{
+	assert_fmt("%.5c", 'a');
+	assert_fmt("%0.5c", 'a');
+	assert_fmt("%1.5c", 'a');
+	assert_fmt("%5.5c", 'a');
+}
+
+//==================================================================
+//   left justify
+//==================================================================
+
+Test(c_tests, l_no_precision)
+{
+	assert_fmt("%-c", 'a');
+	assert_fmt("%-0c", 'a');
+	assert_fmt("%-1c", 'a');
+	assert_fmt("%-5c", 'a');
+}
+
+Test(c_tests, l_zero_precision)
+{
+	assert_fmt("%-.0c", 'a');
+	assert_fmt("%-0.0c", 'a');
+	assert_fmt("%-1.0c", 'a');
+	assert_fmt("%-5.0c", 'a');
+}
+
+Test(c_tests, l_one_precision)
+{
+	assert_fmt("%-.1c", 'a');
+	assert_fmt("%-0.1c", 'a');
+	assert_fmt("%-1.1c", 'a');
+	assert_fmt("%-5.1c", 'a');
+}
+
+Test(c_tests, l_large_precision)
+{
+	assert_fmt("%-.5c", 'a');
+	assert_fmt("%-0.5c", 'a');
+	assert_fmt("%-1.5c", 'a');
+	assert_fmt("%-5.5c", 'a');
+}
+
+//right
+	// a character
+		// no precision
+			// no width
+			// 0 width
+			// right width
+			// large width
+		// 0 precision
+		// enough precision
+		// large precision
+
+//left
