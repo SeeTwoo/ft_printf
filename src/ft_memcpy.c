bc6fad87253f3f345b14ef2732efb4b40a328bf8
SeeTwoo <walti.bee@gmail.com>
2025-12-05 03:56:08 +0100

    trying to make strlen portable lmao

--- src/ft_memcpy.c
+++ src/ft_memcpy.c
@@ -10,31 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <emmintrin.h>
-#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				qword_len;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	while (n > 0 && ((uintptr_t)d % sizeof(size_t)) != 0)
+	{
+		*d++ = *s++;
+		n--;
+	}
+	if (((uintptr_t)s % sizeof(size_t)) == 0)
+	{
+		qword_len = n / sizeof(size_t);
+		while (qword_len > 0)
+		{
+			*(size_t *)d = *(const size_t *)s;
+			d += sizeof(size_t);
+			s += sizeof(size_t);
+			qword_len--;
+		}
+		n %= sizeof(size_t);
+	}
+	while (n > 0)
+	{
+		*d++ = *s++;
+		n--;
+	}
+	return (dest);
+}
 
 /*
-void	*ft_memcpy(void *dest, void const *src, size_t n)
-{
-	__m128i	vect;
-	void	*ret;
-
-	ret = dest;
-	while (n >= 16)
-	{
-		vect = _mm_loadu_si128((__m128i *)src);
-		_mm_storeu_si128((__m128i *)dest, vect);
-		n -= 16;
-		dest += 16;
-		src += 16;
-	}
-	while (n-- > 0)
-		*((char *)dest++) = *((char *)src++);
-	return (ret);
-}
-*/
-
 void	*ft_memcpy(void *dest, void const *src, size_t n)
 {
 	void	*ret;
@@ -44,3 +58,4 @@ void	*ft_memcpy(void *dest, void const *src, size_t n)
 		*(char *)dest++ = *(char *)src++;
 	return (ret);
 }
+*/
